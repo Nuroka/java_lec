d8485243f974ef9ba69b1028030d2ac1d9fd25ab
Nuroka <92858669+Nuroka@users.noreply.github.com>
2023-02-22 15:09:35 +0900

    Add files via upload

---
+++ JavaLec-main/JavaProgramming/src/chap99_etc/_04_numBasball.java
@@ -0,0 +1,21 @@
+package chap99_etc;
+
+import chap99_etc.games.NumBaseball;
+
+public class _04_numBasball {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		NumBaseball nb = new NumBaseball();
+		
+		nb.start();
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ JavaLec-main/JavaProgramming/src/chap99_etc/_05_lotto.java
@@ -0,0 +1,19 @@
+package chap99_etc;
+
+import chap99_etc.games.Lotto;
+
+public class _05_lotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Lotto lt = new Lotto();
+		
+		lt.start();
+		
+		
+		
+		
+		
+	}
+
+}
